ead89750df57ea2bdf64328a2bafa4f98290a2e9
Abhishek Kumar <abhishek022kk@gmail.com>
2021-09-02 01:44:41 +0530

    initial setup of the app done

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import Notifications from "./components/Notifications";
+import Options from "./components/Options";
+import VideoPlayer from "./components/VideoPlayer";
+import Video from "./video.mp4";
+import { AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
+function App() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.wrapper}>
+        <AppBar
+          className={classes.appBar}
+          position="static"
+          color="transparent"
+        >
+          <div className="header">दर्पण-Web Chat App</div>
+        </AppBar>
+        <video className="video" src={Video} loop autoPlay muted></video>
+        <div class="overlay"></div>
+
+        <VideoPlayer />
+        <Options>
+          <Notifications />
+        </Options>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,39 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors"); // for cross origin requests
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+// Sockets are used for real time data connection. That data can be video, audio or messages etc.
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+// Starting the server
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
